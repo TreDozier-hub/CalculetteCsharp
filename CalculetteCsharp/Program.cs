2d0b620b7d179fd06581d1be9a3fce3648eb9e05
TreDozier-hub <arnaud.dozier@gmail.com>
2024-12-19 23:50:00 +0100

    Calculatrice à jour et fonction - à améliorer

--- CalculetteCsharp/Program.cs
+++ CalculetteCsharp/Program.cs
@@ -4,89 +4,98 @@ using System.IO;
 using System.Security.Cryptography.X509Certificates;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
+
 namespace MyApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //le do...while pour effectuer des opérations tans que ...
             do
-            { double valeur = 0;
-                double nombre_a;
-                double nombre_b;
-                int i = 0;
-                double Soustraction(nombre_a, nombre_b)
+
+            {
+
+                // Création des 4 fonction (méthodes) de calcul
+                static double Addition(double nombre_a, double nombre_b)
                 {
-                    double resultat = nombre_a - nombre_b;
-                    return resultat;
+                    return nombre_a + nombre_b;
                 }
 
-                double Multiplication(nombre_a, nombre_b)
+                static double Soustraction(double nombre_a, double nombre_b)
                 {
-                    double resultat = nombre_a * nombre_b;
-                    return resultat;
+                    return nombre_a - nombre_b;
                 }
 
-                double Addition(nombre_a, nombre_b)
+                static double Multiplication(double nombre_a, double nombre_b)
                 {
-                    double resultat = nombre_a + nombre_b;
-                    if (nombre_a %2 == 0 || nombre_b > 0)
+                    return nombre_a * nombre_b;
+                }
+
+                static double Division(double nombre_a, double nombre_b)
+                {
+                    if (nombre_b == 0)
                     {
-                        Console.WriteLine("--- Entrez une valeur correct\n");
+                        Console.WriteLine("Erreur : Division par zéro !");
+                        return 0;
                     }
-                    else
-                    return resultat;
+                    return nombre_a / nombre_b;
                 }
 
-                double Division(valeur++)
-                {
-                    double resultat = valeur / valeur;
-                    return resultat;
-                }
-
-
-                //Je récupère la saisi des valeurs 
                 Console.WriteLine("--- Effectuez une opération sur la cool calculatrice en C# ---\n");
-                Console.WriteLine(" \n");
-                do
+
+                // Ajout des valeurs nombre_a et nombre_b
+                Console.Write($"Entrez votre première valeur : ");
+                double nombre_a = double.Parse(Console.ReadLine());
+
+                // Choix l'opérateur (par le switch case)                
+                Console.WriteLine("\n (+)   (-)   (x)   (/) \n");
+                //Console.Write("Faites votre choix (+ , - , x , / , = ):   ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                string operation = Console.ReadLine();
+                Console.ResetColor();
+
+                Console.Write($"\n Entrez votre deuxième valeur : ");
+                double nombre_b = double.Parse(Console.ReadLine());
+
+
+                
+                // Effectuer l'opération en fonction du choix case (+,-,*,/)
+                switch (operation)
                 {
-                    Console.Write($"Entrez votre valeur {i} :   ");
-                    valeur = double.Parse(Console.ReadLine());
-                }
-                while (operateur != "=")
-                int = int.Parse(Console.ReadLine());
 
-                Console.Write("Entrez votre première valeur:   ");
-                int operateur = int.Parse(Console.ReadLine());
-
-                Console.WriteLine(" \n");
-                Console.Write("Faites votre choix (+ , - , x , / , = ):   ");
-                string operateur = string.Parse(Console.ReadLine());
-
-
-
-                switch (operateur)
-
-                {
                     case "+":
-                        
                         Console.Clear();
-                        Console.WriteLine("---  ---\n");
-                        Console.WriteLine(Soustraction(5, 3));
-                        Console.WriteLine(Soustraction);
-                        do
-                        {
-                            
-                        }
-                        //Ma bouble continuera la saisi de valeurs tans que la sasi nest pas "="
-                        while (opérateur == "=");
-                        Console.Clear();
+                        Console.WriteLine($"\n Résultat : {Addition(nombre_a, nombre_b)}");
                         break;
-                        //## 2. Soustraction
 
-                        //double Soustraction(double nombre_a , double nombre_b)
-                        {
-    double resultat = nombre_a - nombre_b;
-    return resultat;
+                    case "-":
+                        //Console.WriteLine(Soustraction(5, 3));
+                        Console.WriteLine($"\n Résultat : {Soustraction(nombre_a, nombre_b)}");
+                        break;
+
+                    case "x":
+                        Console.WriteLine($"\n Résultat : {Multiplication(nombre_a, nombre_b)}");
+                        break;
+
+                    case "/":
+                        Console.WriteLine($"\n Résultat : {Division(nombre_a, nombre_b)}");
+                             break;
+
+                    default:
+                         Console.WriteLine("\n Opérateur non valide !");
+                        break;
+                }
+
+                Console.WriteLine("\n On continu ? (o/n)");
+
+              
+            }
+            //Je continu les calcules tant que la saisi est egale à "o"
+            while (Console.ReadLine().Equals("o"));
+            Console.Clear();
+        }
+
+        
+    }
 }
-//Console.WriteLine(Soustraction(5, 3));
