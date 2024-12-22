5a15688617ef84d2151d91ad10865113742227a5
TreDozier-hub <arnaud.dozier@gmail.com>
2024-12-22 18:18:25 +0100

    Mise a jour interface et bug (répettition error (+,-,*,/)

--- CalculetteCsharp/Program.cs
+++ CalculetteCsharp/Program.cs
@@ -17,18 +17,7 @@ namespace MyApp
             {
 
 
-                // Création des 4 fonction (méthodes) de calcul
-
-                string OperteurKo(string operation)
-                {
-                    if (operation != "+" || operation != "-" || operation != "*" || operation != "/" || operation != "o")
-                    {
-                        Console.WriteLine("\n \t Saisi un opérateur valide ducon (+)   (-)   (x)   (/) : \n");
-                    }
-                    return operation;
-
-                }
-
+                // Création des 4 fonctions (méthodes) de calcul
                 double Addition(double nombre_a, double nombre_Sup)
                 {
                     return nombre_a + nombre_Sup;
@@ -54,6 +43,34 @@ namespace MyApp
                     return nombre_a / nombre_Sup;
                 }
 
+
+                //Créatoin de la Fonction Cadrage pour entourer les opérateur:
+
+                //void Cadrage(string titre)
+                //{
+                //    Console.Clear();
+
+                //    titre = $"║ {titre} ║";
+                //    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                //    Console.Write("╔");
+                //    for (int i = 0; i < titre.Length - 2; i++)
+                //    {
+                //        Console.Write("═");
+                //    };
+                //    Console.WriteLine("╗");
+                //    Console.WriteLine(titre);
+                //    Console.Write("╚");
+                //    for (int i = 0; i < titre.Length - 2; i++)
+                //    {
+                //        Console.Write("═");
+                //    };
+                //    Console.WriteLine("╝");
+                //    Console.ResetColor();
+                //}
+
+                //Effacé la console après Equals("o") pour sortir
+                Console.Clear();
+
                 Console.WriteLine("--- Effectuez une opération sur la cool calculatrice en C# ---\n");
 
                 // Récupérer la première valeur
@@ -61,29 +78,28 @@ namespace MyApp
                 double resultat = double.Parse(Console.ReadLine());
                 string operation;
 
-                // Je crée une boucle imbriquée pour calculer plusieurs valeurs
+                // Je crée une boucle imbriquée pour calculer plusieurs valeurs a la suite et finir par "="
                 while (true)
                 {
-                    Console.WriteLine("\n (+)   (-)   (x)   (/)   (= pour terminer) \n");
+
+                    Console.WriteLine("\n [+] \t\t[-]\t\t" + "\n [/] \t\t[*] \t\t" + "\n [=](pour terminer) \n");
                     Console.ForegroundColor = ConsoleColor.Red;
                     operation = Console.ReadLine();
                     Console.ResetColor();
-
-
-
 
                     if (operation == "=")
                     {
                         Console.WriteLine($"\n Résultat final : {resultat}");
                         break; // Sortir de la boucle imbriquée
                     }
-                    else if (operation != "+" && operation != "-" && operation != "*" && operation != "/" && operation != "o") ;
-                    
-                    else;
-                    Console.WriteLine("\n \t Saisi un opérateur valide (+)   (-)   (x)   (/) : \n");
-                    operation = Console.ReadLine();
 
-
+                    if (operation != "+" && operation != "-" && operation != "*" && operation != "/" && operation != "o")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\n \t Saisi un opérateur valide (+)   (-)   (x)   (/) : \n");
+                        operation = Console.ReadLine();
+                        Console.ResetColor();
+                    }
 
                     Console.Write($"\n Entrez une autre valeur : ");
                     double nombre_b = double.Parse(Console.ReadLine());
@@ -92,17 +108,22 @@ namespace MyApp
                     switch (operation)
                     {
                         case "+":
+                            //Cadrage("Addition");
                             resultat = Addition(resultat, nombre_b);
                             break;
+
                         case "-":
                             resultat = Soustraction(resultat, nombre_b);
                             break;
+
                         case "x":
                             resultat = Multiplication(resultat, nombre_b);
                             break;
+
                         case "/":
                             resultat = Division(resultat, nombre_b);
                             break;
+
                         default:
                             Console.WriteLine("\n Opérateur non valide !");
                             break;
@@ -112,7 +133,9 @@ namespace MyApp
                 }
 
                 Console.WriteLine("\n On continue ? (o/n)");
+                
             } while (Console.ReadLine().Equals("o"));
+
         }
     }
 }
